17517be010ca649134b5e29a03926e368edbaf68
Harry Prior <harjp@pm.me>
2025-03-05 18:34:00 +0000

    Add direnv-init alias and add get_flake function.

--- home/shell/alias.nix
+++ home/shell/alias.nix
@@ -71,6 +71,15 @@ in {
     pdate = "date -u +%Y-%m-%dT%H_%M_%S";
 
     shell = "nix-shell -p";
+
+    direnv-init = ''
+      echo 'if ! has nix_direnv_version || ! nix_direnv_version 3.0.4; then
+        source_url "https://raw.githubusercontent.com/nix-community/nix-direnv/3.0.4/direnvrc" \
+                   "sha256-DzlYZ33mWF/Gs8DDeyjr8mnVmQGx7ASYqA5WlxwvBG4="
+      fi
+
+      use flake' > .envrc
+    '';
   };
 
   linux = {
