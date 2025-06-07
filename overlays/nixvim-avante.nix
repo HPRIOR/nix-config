d98c691221899ce678c7c19b66b062d767342557
Harry Prior <56833147+HPRIOR@users.noreply.github.com>
2025-06-07 15:06:44 +0100

    pin avante plugin

--- overlays/nixvim-avante.nix
+++ overlays/nixvim-avante.nix
@@ -1,6 +1,14 @@
-{ unstable }:
 self: super: {
   vimPlugins = super.vimPlugins // {
-    avante-nvim = unstable.legacyPackages.${super.system}.vimPlugins.avante-nvim;
+    avante-nvim = super.vimUtils.buildVimPlugin {
+      pname = "avante-nvim";
+      version = "574b0d37";
+      src = super.fetchFromGitHub {
+        owner = "yetone";
+        repo = "avante.nvim";
+        rev = "574b0d37a32fcaf7ade1f76422ac4c8793af0301";
+        hash = "sha256-Mi0BG+RY2qsJcsO2uTxnw0le3AaobaVCplAchyc7/XM=";
+      };
+    };
   };
 }
