8c04702c93b939d37142450bc2ffc7a027cb20e0
Harry Prior <harjp@pm.me>
2026-03-08 20:20:05 +0000

    fix warnings

--- overlays/codex.nix
+++ overlays/codex.nix
@@ -1,5 +1,5 @@
 {codex-cli-nix, unstable}: self: super: let
-  system = super.system or super.stdenv.hostPlatform.system;
+  system = super.stdenv.hostPlatform.system;
 in {
   codex = codex-cli-nix.packages.${system}.default;
 }
