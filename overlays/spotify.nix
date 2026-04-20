35940e8c8585126ddde45f3b227142346fb6f873
Harry Prior <harjp@pm.me>
2026-04-20 08:41:35 +0100

    Update spotify overlay checksum.

--- overlays/spotify.nix
+++ overlays/spotify.nix
@@ -5,7 +5,7 @@ self: prev: {
       then
         prev.fetchurl {
           url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-          hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
+          hash = "sha256-gEZxRBT7Jo2m6pirf+CreJiMeE2mhIkpe9Mv5t0RI58=";
         }
       else oldAttrs.src;
   });
